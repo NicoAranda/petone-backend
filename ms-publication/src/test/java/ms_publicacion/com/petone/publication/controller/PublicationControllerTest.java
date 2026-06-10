f899b5eb9a167869da956f1c3966d9ac3f14c572
NicoAranda <n.aranda@duocuc.cl>
2026-06-10 11:39:25 -0400

    arreglo test

--- ms-publication/src/test/java/ms_publicacion/com/petone/publication/controller/PublicationControllerTest.java
+++ ms-publication/src/test/java/ms_publicacion/com/petone/publication/controller/PublicationControllerTest.java
@@ -14,6 +14,7 @@ import org.mockito.Mockito;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import ms_publicacion.com.petone.publication.config.JwtService;
 import ms_publicacion.com.petone.publication.model.Publication;
 import ms_publicacion.com.petone.publication.service.PublicationService;
 
@@ -21,16 +22,27 @@ public class PublicationControllerTest {
 
     private PublicationService service;
     private PublicationController controller;
+    JwtService jwtService = Mockito.mock(JwtService.class);
 
     @BeforeEach
     void setup() {
         service = Mockito.mock(PublicationService.class);
-        controller = new PublicationController(service);
+        controller = new PublicationController(service, jwtService);
     }
 
     @Test
     void listar_publicaciones_devuelve_ok() {
-        Publication p = new Publication(1L, "Firulais", "Santiago", "Perro", "Macho", "ACTIVA", "Buen perro", new Date(), Arrays.asList("/img/1.jpg"));
+        Publication p = new Publication(
+                1L,
+                6L,
+                "Firulais",
+                "Santiago",
+                "Perro",
+                "Macho",
+                "ACTIVA",
+                "Buen perro",
+                new Date(),
+                Arrays.asList("/img/1.jpg"));
         List<Publication> list = List.of(p);
         when(service.viewAll()).thenReturn(list);
 
@@ -41,7 +53,8 @@ public class PublicationControllerTest {
 
     @Test
     void obtener_publicacion_por_id_devuelve_ok() {
-        Publication p = new Publication(2L, "Rex", "Valparaiso", "Perro", "Macho", "ACTIVA", "Amigable", new Date(), Arrays.asList("/img/dog.jpg"));
+        Publication p = new Publication(2L, 6L, "Rex", "Valparaiso", "Perro", "Macho", "ACTIVA", "Amigable", new Date(),
+                Arrays.asList("/img/dog.jpg"));
         when(service.viewById(2L)).thenReturn(p);
 
         ResponseEntity<Publication> resp = controller.obtener(2L);
@@ -51,8 +64,10 @@ public class PublicationControllerTest {
 
     @Test
     void actualizar_publicacion_devuelve_ok() {
-        Publication input = new Publication(null, "Camila", "Santiago", "Gato", "Hembra", "ACTIVA", "Actualizada", new Date(), Arrays.asList());
-        Publication updated = new Publication(3L, "Camila", "Santiago", "Gato", "Hembra", "ACTIVA", "Actualizada", new Date(), Arrays.asList());
+        Publication input = new Publication(null, 6L, "Camila", "Santiago", "Gato", "Hembra", "ACTIVA", "Actualizada",
+                new Date(), Arrays.asList());
+        Publication updated = new Publication(3L, 6L, "Camila", "Santiago", "Gato", "Hembra", "ACTIVA", "Actualizada",
+                new Date(), Arrays.asList());
         when(service.updateById(3L, input)).thenReturn(updated);
 
         ResponseEntity<Publication> resp = controller.actualizar(3L, input);
